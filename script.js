74647ffd198028b2dfedc1b3299e712d6b340a15
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-02 22:28:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-$('.button_filter').on('click', function() {
-  $('.logo').addClass('is-hidden');
+$(document).ready(function() {
+    // Select all elements with the class 'products_item-img'
+    $('.products_item-img').each(function(index) {
+        // Apply initial CSS styles to set the start state of each element
+        $(this).css({
+            'margin-left': '-50%',
+            'width': '0%'
+        });
+
+        // Animate each element with a delay using the index to create a staggered effect
+        $(this).delay(300 * index).animate({
+            'margin-left': '0%',
+            'width': '100%'
+        }, 1000); // Adjust duration as needed (in milliseconds)
+    });
 });
