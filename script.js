16531aa1967bb2ff441ce73d9000ce49c1e5cb52
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-02 22:45:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 $(document).ready(function() {
-    // Function to check if an element is in the viewport
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
+    var animated = false;
 
-    // Function to handle the animation of elements
-    function animateElements() {
-        $(".products_item-img").each(function(index) {
-            var el = this;
-            if (isElementInViewport(el) && !$(el).hasClass("is-visible")) {
-                $(el).addClass("is-visible").delay(150 * index).animate({
-                    "margin-left": 0,
-                    "width": "100%"
-                }, 1000); // Adjust the animation speed (1000ms for 1 second)
-            }
-        });
-    }
+    $(window).on('scroll', function() {
+        if (!animated) {
+            $('.products_item-img').each(function(index) {
+                var elementTop = $(this).offset().top;
+                var viewportBottom = $(window).scrollTop() + $(window).height();
 
-    // Trigger the animation when the page loads and on scroll
-    $(window).on("load scroll", function() {
-        animateElements();
+                if (elementTop < viewportBottom) {
+                    $(this).css('transition-delay', index * 0.2 + 's');
+                    $(this).css('margin-left', '0');
+                    $(this).css('width', '100%');
+                }
+            });
+            animated = true; // to ensure the animation only happens once
+        }
     });
 });
