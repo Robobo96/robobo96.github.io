07aba7fb8512143d85fff33519c7f7fd803a4ecf
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-02 22:13:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Get all elements with the class 'button_filter'
-const buttons = document.querySelectorAll('.button_filter');
-
-// Get all elements with the class 'logo'
-const logos = document.querySelectorAll('.logo');
-
-// Function to add 'is-hidden' class to elements with class 'logo'
-function hideLogos() {
-    logos.forEach(logo => {
-        logo.classList.add('is-hidden');
-    });
-}
-
-// Add click event to each button
-buttons.forEach(button => {
-    button.addEventListener('click', hideLogos);
+$('.button_filter').on('click', function() {
+  $('.logo').addClass('is-hidden');
 });
