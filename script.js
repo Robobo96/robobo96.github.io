3d563f88f5478494422ff567c4574f60508f3678
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-02 23:17:29 +0100

    TR.icks staggered animation

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-$(document).ready(function() {
-    var animated = false;
+<script src="https://cdnjs.cloudflare.com/ajax/libs/protonet-jquery.inview/1.1.2/jquery.inview.min.js"></script>
+<script> 
 
-    $(window).on('scroll', function() {
-        if (!animated) {
-            $('.products_item-img').each(function(index) {
-                var elementTop = $(this).offset().top;
-                var viewportBottom = $(window).scrollTop() + $(window).height();
+// © Code by T.RICKS, https://www.tricksdesign.com/
+// You have the license to use this code in your projects but not redistribute it to others
 
-                if (elementTop < viewportBottom) {
-                    $(this).css('transition-delay', index * 0.2 + 's');
-                    $(this).css('margin-left', '0');
-                    $(this).css('width', '100%');
-                }
-            });
-            animated = true; // to ensure the animation only happens once
-        }
-    });
+// Stagger your animation
+ $(".trigger").each(function (i) {
+    var $item = $(this); 
+    setTimeout(function() { 
+      $item.click();
+    }, 200*i);
+    // delays the next animation by 200 milliseconds
+  });
+ 
+
+// TRIGGERS
+
+// Wait before playing animation
+setTimeout(() => {  
+// Put the play below this line
+}, 800);
+
+
+// Play animation when something is clicked
+$( ".your-button-class" ).click(function() {
+	// Put the play below this line
 });
+
+
+// Play animation when hovered in
+$( ".your-button-class" ).mouseenter(function() {
+		// Put the play below this line
+});
+
+// Play animation when hovered out
+$( ".your-button-class" ).mouseleave(function() {
+		// Put the play below this line
+});
+
+// Play animation when scrolled into view
+$('#heading-container').one('inview', function(event, isInView) {
+  if (isInView) {
+		// Put the play below this line
+  } else {
+  }
+});
+  
+ 
+</script>
