ae037a3eb534e9ccf0ade217fef2cd617feaa9c6
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-03 14:11:04 +0100

    Added code to make 8th and 9th item large

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ $(document).ready(function() {
             var $productsGrid = $('.products_grid');
             var $newProductItem = $('<div class="products_item"></div>');
 
-            // Select and insert the new item at the specified indexes: 2, 10, and 12
             $productsGrid.children().eq(2).before($newProductItem.clone()); // Index 2
             $productsGrid.children().eq(10).before($newProductItem.clone()); // Index 10
             $productsGrid.children().eq(12).before($newProductItem); // Index 12
+
+            // Apply CSS directly to the 8th and 9th child elements to make them span 2 columns and 2 rows
+            $productsGrid.children().eq(7).css({
+                'grid-column': 'auto / span 2',
+                'grid-row': 'auto / span 2'
+            });
+            $productsGrid.children().eq(8).css({
+                'grid-column': 'auto / span 2',
+                'grid-row': 'auto / span 2'
+            });
         });
