d25c9ff3c3c20abb85a38f73e86bdd311e3adeac
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-03 13:49:50 +0100

    Completely different code

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/protonet-jquery.inview/1.1.2/jquery.inview.min.js"></script>
-<script> 
+// Wait for the HTML document to be fully loaded and ready
+$(document).ready(function() {
 
-// © Code by T.RICKS, https://www.tricksdesign.com/
-// You have the license to use this code in your projects but not redistribute it to others
- 
+    // Define the grid cells with the class name 'products_grid'
+    var gridCells = $('.products_grid');
 
-// TRIGGERS
+    // Loop through each 'products_grid' element found on the page
+    gridCells.each(function(index, element) {
 
-// Wait before playing animation
-setTimeout(() => {  
-// Put the play below this line
-}, 800);
+        // Find all the children cells of the current 'products_grid' element
+        var children = $(element).children();
 
+        // Loop through each child cell of the grid
+        children.each(function(childIndex) {
 
-// Play animation when something is clicked
-$( ".your-button-class" ).click(function() {
-	// Put the play below this line
+            // Check if the current child cell is the 3rd, 11th, or 13th (0-indexed)
+            if ((childIndex + 1) % 3 === 0 && (childIndex + 1) !== 12) {
+                // Create a new empty div element
+                var newDiv = $('<div></div>');
+
+                // Add the class 'products_item' to the new div
+                newDiv.addClass('products_item');
+
+                // Append the new div to the current child cell
+                $(this).append(newDiv);
+            }
+        });
+    });
 });
-
-
-// Play animation when hovered in
-$( ".your-button-class" ).mouseenter(function() {
-		// Put the play below this line
-});
-
-// Play animation when hovered out
-$( ".your-button-class" ).mouseleave(function() {
-		// Put the play below this line
-});
-
-// Play animation when scrolled into view
-$('#heading-container').one('inview', function(event, isInView) {
-  if (isInView) {
-		// Put the play below this line
-	  	// Stagger your animation
- 		$(".products_item-img-wrapper").each(function (i) {
-	    		var $item = $(this); 
-	    		setTimeout(function() { 
-	      			$item.click();
-	    		}, 200*i);
-    			// delays the next animation by 200 milliseconds
-  });
-  } else {
-  }
-});
-  
- 
-</script>
