17ce0d9805efbd33f671365f6dd3761c1550e1f8
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-03 16:42:05 +0100

    Update script.js
    This revised code combines the loop for inserting empty divs at different positions (3, 11, 13) into one loop using an array to store these positions. This way, it minimizes repetition and enhances the maintainability of the code.

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
 $(document).ready(function() {
-            var $productsGrid = $('.products_grid');
-            var $newProductItem = $('<div class="products_item"></div>');
+    var $productsGrid = $('.products_grid');
+    var $newProductItem = $('<div class="products_item"></div>');
 
-            // Apply CSS directly to the 8th and 9th child elements to make them span 2 columns and 2 rows
-            $productsGrid.children().eq(7).css({
-                'grid-column': 'auto / span 2',
-                'grid-row': 'auto / span 2'
-            });
-            $productsGrid.children().eq(8).css({
-                'grid-column': 'auto / span 2',
-                'grid-row': 'auto / span 2'
-            });
-            
-            // Check and insert empty divs at positions 3, 15, 26, etc., if the grid has more than those indices' number of children
-            for (var i = 3; i < $productsGrid.children().length; i += 12) {
-                        if ($productsGrid.children().length > i) {
-                                    $productsGrid.children().eq(i - 1).before($newProductItem.clone());
-                        }
-            }
-            
-            // Check and insert empty divs at positions 11, 23, 35, etc., if the grid has more than those indices' number of children
-            for (var i = 11; i < $productsGrid.children().length; i += 12) {
-                        if ($productsGrid.children().length > i) {
-                                    $productsGrid.children().eq(i - 1).before($newProductItem.clone());
-                        }
-            }
+    // Apply CSS directly to the 8th and 9th child elements to make them span 2 columns and 2 rows
+    $productsGrid.children().eq(7).add($productsGrid.children().eq(8)).css({
+        'grid-column': 'auto / span 2',
+        'grid-row': 'auto / span 2'
+    });
 
-            // Check and insert empty divs at positions 13, 25, 37, etc., if the grid has more than those indices' number of children
-            for (var i = 13; i < $productsGrid.children().length; i += 12) {
-                        if ($productsGrid.children().length > i) {
-                                    $productsGrid.children().eq(i - 1).before($newProductItem.clone());
-                        }
+    // Define positions to insert empty divs
+    var positions = [3, 11, 13]; // Positions to insert empty divs
+
+    // Loop through each position and insert an empty div if the grid has more children
+    positions.forEach(function(position) {
+        for (var i = position; i < $productsGrid.children().length; i += 12) {
+            if ($productsGrid.children().length > i) {
+                $productsGrid.children().eq(i - 1).before($newProductItem.clone());
             }
-        });
+        }
+    });
+});
