19d03ed526dee3e98fc28787ac2008b3899cd70e
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-03 16:45:45 +0100

    Update script.js
    This updated code includes a loop to apply the CSS attributes to the 8th and 9th child elements and their subsequent elements in a similar pattern, just like the loops that insert the empty divs. The loop iterates through the grid structure, selecting elements at positions 8, 20, 32, etc., applying the specified CSS properties to these elements.

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
     var $productsGrid = $('.products_grid');
     var $newProductItem = $('<div class="products_item"></div>');
 
-    // Apply CSS directly to the 8th and 9th child elements to make them span 2 columns and 2 rows
-    $productsGrid.children().eq(7).add($productsGrid.children().eq(8)).css({
-        'grid-column': 'auto / span 2',
-        'grid-row': 'auto / span 2'
-    });
+    // Apply CSS directly to the 8th and 9th child elements and subsequent ones in the same pattern
+    for (var i = 7; i < $productsGrid.children().length; i += 12) {
+        $productsGrid.children().eq(i).add($productsGrid.children().eq(i + 1)).css({
+            'grid-column': 'auto / span 2',
+            'grid-row': 'auto / span 2'
+        });
+    }
 
     // Define positions to insert empty divs
     var positions = [3, 11, 13]; // Positions to insert empty divs
