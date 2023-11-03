f5c6942d81c5b45507cc7228d73c9f6c227885f3
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-03 14:00:41 +0100

    Place empty div in 3rd 11th and 13th

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
             var $productsGrid = $('.products_grid');
             var $newProductItem = $('<div class="products_item"></div>');
-            
-            // Select the third child element (index 2) in the products_grid and insert the new item before it
-            $productsGrid.children().eq(2).before($newProductItem);
+
+            // Select and insert the new item at the specified indexes: 2, 10, and 12
+            $productsGrid.children().eq(2).before($newProductItem.clone()); // Index 2
+            $productsGrid.children().eq(10).before($newProductItem.clone()); // Index 10
+            $productsGrid.children().eq(12).before($newProductItem); // Index 12
         });
