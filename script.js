b0076fa6076f8eac62a7f7d73678cdf085a66974
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-03 14:13:37 +0100

    Switched order, so right items are editted

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ $(document).ready(function() {
             var $productsGrid = $('.products_grid');
             var $newProductItem = $('<div class="products_item"></div>');
 
-            $productsGrid.children().eq(2).before($newProductItem.clone()); // Index 2
-            $productsGrid.children().eq(10).before($newProductItem.clone()); // Index 10
-            $productsGrid.children().eq(12).before($newProductItem); // Index 12
-
             // Apply CSS directly to the 8th and 9th child elements to make them span 2 columns and 2 rows
             $productsGrid.children().eq(7).css({
                 'grid-column': 'auto / span 2',
@@ -15,4 +11,8 @@ $(document).ready(function() {
                 'grid-column': 'auto / span 2',
                 'grid-row': 'auto / span 2'
             });
+            
+            $productsGrid.children().eq(2).before($newProductItem.clone()); // Index 2
+            $productsGrid.children().eq(10).before($newProductItem.clone()); // Index 10
+            $productsGrid.children().eq(12).before($newProductItem); // Index 12
         });
