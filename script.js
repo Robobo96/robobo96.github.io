c0c1de21e6381591b98f09f29934cd51d9043aab
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-03 16:51:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ $(document).ready(function() {
     var $productsGrid = $('.products_grid');
     var $newProductItem = $('<div class="products_item"></div>');
 
+    // Define positions to insert CSS for larger images
+    var largerimgs = [8, 9]; // Positions to insert CSS
+    
     // Apply CSS directly to the 8th and 9th child elements and subsequent ones in the same pattern
-    for (var i = 7; i < $productsGrid.children().length; i += 12) {
-        $productsGrid.children().eq(i).add($productsGrid.children().eq(i + 1)).css({
-            'grid-column': 'auto / span 2',
-            'grid-row': 'auto / span 2'
-        });
-    }
+    largerimgs.forEach(function(largerimg) {
+        for (var i = largerimg; i < $productsGrid.children().length; i += 12) {
+            $productsGrid.children().eq(i).add($productsGrid.children().eq(i + 1)).css({
+                'grid-column': 'auto / span 2',
+                'grid-row': 'auto / span 2'
+            });
+        }
+    });
 
     // Define positions to insert empty divs
-    var positions = [3, 11, 13]; // Positions to insert empty divs
+    var emptydivs = [3, 11, 13]; // Positions to insert empty divs
 
     // Loop through each position and insert an empty div if the grid has more children
-    positions.forEach(function(position) {
-        for (var i = position; i < $productsGrid.children().length; i += 12) {
+    emptydivs.forEach(function(emptydiv) {
+        for (var i = emptydiv; i < $productsGrid.children().length; i += 12) {
             if ($productsGrid.children().length > i) {
                 $productsGrid.children().eq(i - 1).before($newProductItem.clone());
             }
