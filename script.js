bace409d01127bac4fd3b95797e4627e85ce9b6a
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-03 16:06:59 +0100

    added if statement

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ $(document).ready(function() {
                 'grid-column': 'auto / span 2',
                 'grid-row': 'auto / span 2'
             });
-            
-            $productsGrid.children().eq(2).before($newProductItem.clone()); // Index 2
+            if ($productsGrid.children().length < 3) {
+                        $productsGrid.children().eq(2).before($newProductItem.clone()); // Index 2
+            }
             $productsGrid.children().eq(10).before($newProductItem.clone()); // Index 10
             $productsGrid.children().eq(12).before($newProductItem); // Index 12
         });
