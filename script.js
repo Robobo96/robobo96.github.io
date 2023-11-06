ad1479c609c578f946e576e72c0762adfa828f90
Robobo96 <31900639+Robobo96@users.noreply.github.com>
2023-11-06 21:13:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,57 +14,21 @@ $(document).ready(function() {
             });
         }
     });
-
-    // Check and insert empty divs at positions 3, 15, 26, etc., if the grid has more than those indices' number of children
-    /*for (var i = 3; i < $productsGrid.children().length; i += 12) {
-                if ($productsGrid.children().length > i) {
-                            $productsGrid.children().eq(i - 1).before($newProductItem.clone());
-                }
-    }
     
-    // Check and insert empty divs at positions 11, 23, 35, etc., if the grid has more than those indices' number of children
-    /*for (var i = 11; i < $productsGrid.children().length; i += 14) {
-                if ($productsGrid.children().length > i) {
-                            $productsGrid.children().eq(i - 1).before($newProductItem.clone());
-                }
-    }
-
-    // Check and insert empty divs at positions 13, 25, 37, etc., if the grid has more than those indices' number of children
-    for (var i = 12; i < $productsGrid.children().length; i += 12) {
-                if ($productsGrid.children().length > i) {
-                            $productsGrid.children().eq(i - 1).before($newProductItem.clone());
-                }
-    }
-
+    // Check and insert empty divs at positions 3, 15, 26, etc., if the grid has more than those indices' number of children
     for (var i = 3; i < $productsGrid.children().length; i += 12) {
         $productsGrid.children().eq(i - 1).before($newProductItem.clone());
     }
 
-    for (var i = 11; i < $productsGrid.children().length; i += 12) {
+    // Check and insert empty divs at positions 11, 23, 35, etc., if the grid has more than those indices' number of children
+    for (var i = 11; i < $productsGrid.children().length; i += 13) {
         $productsGrid.children().eq(i - 1).before($newProductItem.clone());
     }
 
-    for (var i = 13; i < $productsGrid.children().length; i += 12) {
+    // Check and insert empty divs at positions 13, 25, 37, etc., if the grid has more than those indices' number of children
+    for (var i = 13; i < $productsGrid.children().length; i += 14) {
         $productsGrid.children().eq(i - 1).before($newProductItem.clone());
-    }*/
-
-for (var i = 3; i < $productsGrid.children().length; i += 12) {
-    var $clonedItem = $newProductItem.clone();
-    $clonedItem.text('Index: ' + i + ' - ' + $clonedItem.text());
-    $productsGrid.children().eq(i - 1).before($clonedItem);
-}
-
-for (var i = 11; i < $productsGrid.children().length; i += 13) {
-    var $clonedItem = $newProductItem.clone();
-    $clonedItem.text('Index: ' + i + ' - ' + $clonedItem.text());
-    $productsGrid.children().eq(i - 1).before($clonedItem);
-}
-
-for (var i = 13; i < $productsGrid.children().length; i += 14) {
-    var $clonedItem = $newProductItem.clone();
-    $clonedItem.text('Index: ' + i + ' - ' + $clonedItem.text());
-    $productsGrid.children().eq(i - 1).before($clonedItem);
-}
+    }
 
 
 });
